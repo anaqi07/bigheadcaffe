f164966477e86d5dc88b7fdfef46b3c78db9b83c
anaqi07 <a.naqi.07@hotmail.com>
2017-06-03 17:13:18 +0300

    form for create edit coffee

--- mainapp/models.py
+++ mainapp/models.py
@@ -29,14 +29,14 @@ class Coffee(models.Model):
     user=models.ForeignKey(User)
     name=models.CharField(max_length=225)
     bean=models.ForeignKey(Bean)
-    syrup=models.ForeignKey(Syrup)
-    powder=models.ForeignKey(Powder)
+    syrup=models.ManyToManyField(Syrup, blank=True)
+    powder=models.ManyToManyField(Powder, blank=True)
     roast=models.ForeignKey(Roast)
     espresso_shots=models.PositiveIntegerField(default=1)
     water=models.FloatField(default=0)
     steamed_milk=models.BooleanField(default=False)
     foam=models.FloatField(default=0)
-    extra_instruction=models.TextField()
+    extra_instruction=models.TextField(blank=True)
 
     def __str__(self):
         return self.name
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import *
+from django.contrib.auth.models import User
+
+def home(request):
+    return render(request, 'home.html',{})
+
+def createCoffee(request):
+    context={}
+    if request.method=="POST":
+        form=CoffeeForm(request.POST)
+        context['form']=form
+        if form.is_valid():
+            coffee=form.save(commit=False)
+            coffee.user=request.user
+            coffee.save()
+            form.save_m2m()
+            return redirect("home")
+        else:
+            return render(request, 'createCoffee.html',context)
+    else:
+        form=CoffeeForm()
+        context['form']=form
+        return render(request, 'createCoffee.html', context)
+
+def editCoffee(request, coffee_id):
+    context={}
+    coffee=Coffee.objects.get(id=coffee_id)
+    context['coffee']=coffee
+    if request.method=="POST":
+        form=CoffeeForm(request.POST, instance=coffee)
+        context['form']=form
+        if form.is_valid():
+            coffee=form.save(commit=False)
+            coffee.user=request.user
+            coffee.save()
+            form.save_m2m()
+            return redirect("home")
+        else:
+            return render(request, 'editCoffee.html',context)
+    else:
+        form=CoffeeForm(instance=coffee)
+        context['form']=form
+        return render(request, 'editCoffee.html', context)
